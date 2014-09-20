a430ada36ce32f3c27b0625f350b0f0a864801f9
MaximeHeckel <heckelmaxime@gmail.com>
2014-09-20 12:46:36 +0200

    add map controller

--- public/js/core.js
+++ public/js/core.js
@@ -14,17 +14,37 @@ function mainController($scope, $http){
   $http.get('/api/lastrun')
     .success(function(data){
       $scope.lastrun = data;
-      $scope.path = data.path;
     })
     .error(function(data){
       console.log('Error: ' + data);
     });
+}
 
-    angular.extend($scope, {
-                london: {
-                    lat: 51.505,
-                    lng: -0.09,
-                    zoom: 4
+function mapController($scope,$http){
+  angular.extend($scope, {
+              london: {
+                  lat: 51.505,
+                  lng: -0.09,
+                  zoom: 4
+              },
+              path: {
+                  p1: {
+                      color: 'red',
+                      weight: 8,
+                      latlngs: [
+                          { lat: 51.50, lng: -0.082 },
+                          { lat: 48.83, lng: 2.37 },
+                          { lat: 41.91, lng: 12.48 }
+                      ]
+                  },
+                  p2: {
+                      color: 'green',
+                      weight: 8,
+                      latlngs: [
+                          { lat: 48.2083537, lng: 16.3725042 },
+                          { lat: 48.8534, lng: 2.3485 }
+                      ]
+                  }
                 }
-            });
+          });
 }
