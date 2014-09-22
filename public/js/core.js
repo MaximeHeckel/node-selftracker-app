47e0f258d727d98e71583acdf49c6542c568fa27
MaximeHeckel <heckelmaxime@gmail.com>
2014-09-22 17:04:18 +0200

    custom mapbox style

--- public/js/core.js
+++ public/js/core.js
@@ -2,15 +2,26 @@ var selftracking = angular.module('selftracking',["leaflet-directive"])
 
   selftracking.service('positionService', function($http){
     var lastrunpos = [];
+    var avglat = 0;
+    var avglng = 0;
+    var avg = []
     $http.get('/api/lastrun')
       .success(function(data){
         angular.forEach(data.path, function (value) {
          lastrunpos.push({lat : value.latitude, lng : value.longitude});
+        avglat += value.latitude
+        avglng += value.longitude
+        meanlat = avglat/data.path.length
+        meanlng = avglng/data.path.length
        });
+       avg.push({lat: meanlat, lng: meanlng});
      });
      return {
-       getPos : function() {
+      getPos : function() {
         return lastrunpos;
+      },
+      getAvg : function(){
+        return avg;
       }
      }
   });
@@ -36,7 +47,7 @@ function mainController($scope, $http){
 function mapController($scope, positionService){
   angular.extend($scope, {
               defaults: {
-                tileLayer : 'http://{s}.tiles.mapbox.com/v3/sarasafavi.hjegnofh/{z}/{x}/{y}.png',
+                tileLayer : 'http://{s}.tiles.mapbox.com/v3/maximeheckel.jimj6jon/{z}/{x}/{y}.png',
                 scrollWheelZoom: false,
                 zoomControl : false,
                 doubleClickZoom : false,
@@ -45,14 +56,14 @@ function mapController($scope, positionService){
                 attributionControl : false
               },
               run: {
-                  lat : 48.58,
-                  lng : 7.737,
+                  lat : 48.582,
+                  lng : 7.74,
                   zoom: 13
               },
               path: {
                   p1: {
-                      color: 'blue',
-                      weight: 4,
+                      color: '#8AAEFF',
+                      weight: 3,
                       zoom : 8,
                       latlngs: positionService.getPos()
                   }
